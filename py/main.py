7245e20be62664df200f0d9aa2c243e0d2e9c0a9
wolfgames <36765@ma-web.nl>
2023-10-16 10:03:44 +0200

    goose

--- py/main.py
+++ py/main.py
@@ -6,7 +6,7 @@ oldgreeting.greet()
 
 
 def basicHaiku():
-    return ["Toward those short trees","We saw a batman descending","On a day in spring."]
+    return ["Toward those short trees","We saw a planet descending","On a day in spring."]
 
 #zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
 
